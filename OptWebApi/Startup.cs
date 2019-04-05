4473d0bf2bd372e43cecec020cb689f2ce65fa4e
SanSYS <i.sansys@gmail.com>
2019-04-05 22:43:39 +0500

    Remove Razor

--- OptWebApi/Startup.cs
+++ OptWebApi/Startup.cs
@@ -18,7 +18,9 @@ namespace OptWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvcCore()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                    .AddJsonFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
